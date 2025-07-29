14baa9e506dbbc5706102e0bc9e1859cf8bc63dc
Tyler Hawkins <jth24e@fsu.edu>
2025-07-29 15:54:09 -0400

    ufo_functions.hpp

---
+++ ufo_functions.hpp
@@ -0,0 +1,7 @@
+#include <vector>
+#include <string>
+
+void display_misses(int misses);
+void greet();
+void end_game(std::string answer, std::string codeword);
+void display_status(std::vector<char> incorrect, std::string answer);
